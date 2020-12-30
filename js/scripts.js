adc27f2d7fbda49ad0ede03d8740c335ce68c150
alexburh95 <alexburh@gmail.com>
2020-12-30 16:25:54 +0300

    Сил нет

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,19 @@ let sticky = new Sticky('.header_main');
 
 
 
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+anchors.forEach((anchor) => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'end'
+    })
+  
+    // window.scrollBy(0, -10)
+  })
+})

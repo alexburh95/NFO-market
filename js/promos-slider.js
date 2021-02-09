47dad3c5252247ed933aefc85ce0cb6ef9b444ef
Bogdan Bryzh <bogdan.bryzh@gmail.com>
2021-02-09 16:41:15 +0200

    fixed slider

--- js/promos-slider.js
+++ js/promos-slider.js
@@ -2,10 +2,15 @@
   new Swiper('.promo-swiper', {
     direction: 'horizontal',
     loop: true,
-    slidesPerView: 2,
+    slidesPerView: 1,
+    spaceBetween: 30,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
     breakpoints: {
       600: {
-        slidesPerView: 1,
+        slidesPerView: 2,
       },
     },
   })
